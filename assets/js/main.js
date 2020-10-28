7f068489a7580cffdb8d6af83b6bb2cc037888b3
emanmoq <42995029+emanmoq@users.noreply.github.com>
2020-10-29 01:56:49 +0300

    v

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,11 @@
 jQuery(function ($) {
-  $(".login").click(function(){
+  $(".loginclick").click(function(){
     $(".loginBox").css("display","block");
   })
   $(".loginBox .close").click(function(){
     $(".loginBox").css("display","none");
   })
-  $(window).scroll(function () {
-      
+  $(window).scroll(function () {  
     scrollTop = $(window).scrollTop();
 
     if (scrollTop >($('.headerImg').height()+ 200)) {
@@ -18,43 +17,7 @@ jQuery(function ($) {
     }
 
 });
-
-    $('.successSlider').owlCarousel({
-        autoplay: true,
-        loop: true,
-        margin: 18,
-        responsive: {
-          0: {
-            items: 1,
-           margin:0
-  
-          },
-          600: {
-            items: 2,
-          },
-          1000: {
-            items: 3,
-          }
-        }
-      });
-      $('.membersSlider').owlCarousel({
-        autoplay: true,
-        loop: true,
-        margin: 20,
-        responsive: {
-          0: {
-            items: 1,
-           margin:0
-  
-          },
-          600: {
-            items: 3,
-          },
-          1000: {
-            items: 4,
-          }
-        }
-      });
-
 });
+
+
 
